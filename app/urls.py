da91b2fa0de07c6c3daf8389d03c2e6a53fb2da9
kazuma1210 <kazuma200401210@outlook.jp>
2024-12-03 15:39:19 +0900

    変更内容の説明

---
+++ app/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LoginView
+
+
+urlpatterns = [
+    path('', views.home, name='home'),  # ホーム画面の表示
+    path('self_analysis/', views.self_analysis, name='self_analysis'),  # 自己分析開始
+    path('community_forum/', views.community_forum, name='community_forum'),  # コミュニティフォーラム
+    path('user_info/', views.user_info, name='user_info'),  # ユーザー情報
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('logout/', LogoutView.as_view(template_name='logout_confirm.html'), name='logout'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+]
---
+++ app/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+
+
+def home(request):
+    return render(request, 'home.html')  # ホーム画面
+    context = {
+        'is_authenticated': request.user.is_authenticated,
+        'username': request.user.username if request.user.is_authenticated else 'ゲスト',
+    }
+    return render(request, 'home.html', context)
+
+
+def self_analysis(request):
+    return render(request, 'self_analysis.html')  # 自己分析開始画面
+
+def community_forum(request):
+    return render(request, 'community_forum.html')  # コミュニティフォーラム画面
+
+def user_info(request):
+    return render(request, 'user_info.html')  # ユーザー情報画面
