d3c9eab6c5409ea07ae658ab26288a7cd1fa4342
kazuma1210 <kazuma20041210@outlook.jp>
2024-12-09 14:36:33 +0900

    あ

--- app/urls.py
+++ app/urls.py
@@ -7,7 +7,7 @@ from django.contrib.auth.views import LoginView
 urlpatterns = [
     path('', views.home, name='home'),  # ホーム画面の表示
     path('self_analysis/', views.self_analysis, name='self_analysis'),  # 自己分析開始
-    path('community_forum/', views.community_forum, name='community_forum'),  # コミュニティフォーラム
+    path('community_thread/', views.community_thread, name='community_thread'),  # コミュニティフォーラム
     path('user_info/', views.user_info, name='user_info'),  # ユーザー情報
     path('logout/', LogoutView.as_view(), name='logout'),
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
