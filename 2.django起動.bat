da00025e353dc4789cf3795af8f742e69df196b1
kazuma1210 <kazuma20041210@outlook.jp>
2025-01-14 11:46:27 +0900

    メール認証機能実装 ログイン周り完成 コミュフォ9割完成 自己分析未完成

---
+++ 2.django起動.bat
@@ -0,0 +1,22 @@
+@echo off
+REM 作業ディレクトリ（D:\sotuken\it_project）に移動
+cd /d E:\sotuken\it_project
+
+REM 必要なパッケージのインストール
+echo Installing dependencies from requirements.txt...
+pip install -r requirements.txt
+
+REM マイグレーションの実行
+echo Running migrations...
+python manage.py makemigrations
+python manage.py migrate
+
+REM 開発用サーバーを起動
+echo Starting the development server...
+python manage.py runserver
+
+REM 完了メッセージ
+echo Django development server is running.
+
+REM バッチ処理終了
+exit
